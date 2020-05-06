b97c99d16091e8dac131f88a2adfb7a9eb0931b3
alan <poson.alan@gmail.com>
2020-05-06 17:25:18 +0200

    push theme perso

---
+++ wthemeperso/wp-content/themes/themetest/footer.php
@@ -0,0 +1,14 @@
+</div> <!-- /.container -->
+<footer class="blog-footer">
+<p>Par <a href="https://www.wppourlesnuls.com/"> Alan le codeur </a>.</p>
+</footer>
+<script
+src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script
+src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js">
+</script>
+// <?php wp_footer(); ?>
+<!-- // Cette fonction permet à des plugins et à différentes
+// fonctions d’ajouter des éléments à cet endroit précis. -->
+</body>
+</html>
---
+++ wthemeperso/wp-content/themes/themetest/sidebar.php
@@ -0,0 +1,14 @@
+
+<!-- barre lateral ( recherche , categorie , article ); -->
+
+
+<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+<div class="sidebar-module sidebar-module-inset">
+<h4>À Propos</h4>
+<p><?php the_author_meta( 'description' ); ?> </p>
+<h4>Archives</h4>
+<ol class="list-unstyled">
+<?php wp_get_archives( 'type=monthly' ); ?>
+</ol>
+</div>
+</div><!-- /.blog-sidebar -->
